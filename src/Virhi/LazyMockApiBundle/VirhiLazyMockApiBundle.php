f44178bc0b15a24b257912995dd1672207eab948
virhi <vincent.rhino@gmail.com>
2015-01-17 18:04:02 +0100

    add virhi admin dependencies

--- src/Virhi/LazyMockApiBundle/VirhiLazyMockApiBundle.php
+++ src/Virhi/LazyMockApiBundle/VirhiLazyMockApiBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class VirhiLazyMockApiBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'VirhiAdminBundle';
+    }
 }
