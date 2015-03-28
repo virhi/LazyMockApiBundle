e905f73b6e4f2a55bf3f3621be4d8dfe3ea09b51
virhi <vincent.rhino@gmail.com>
2015-03-28 14:03:34 +0100

    wip integration admin lte2

--- src/Virhi/LazyMockApiBundle/Controller/UiController.php
+++ src/Virhi/LazyMockApiBundle/Controller/UiController.php
@@ -12,6 +12,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Virhi\LazyMockApiBundle\Mock\Application\Context\Query\ListMockContext;
 use Virhi\LazyMockApiBundle\Mock\Application\Context\Query\MockContext;
 use Virhi\LazyMockApiBundle\Mock\Infrastructure\Entity\Mock;
+use Virhi\AdminBundle\DTO\MenuItem;
+use Virhi\AdminBundle\DTO\User;
 
 class UiController extends Controller
 {
@@ -34,5 +36,35 @@ class UiController extends Controller
         $result  = $this->get('virhi_lazy_mock_api.application.query.mock')->execute($context);
 
         return $this->render('VirhiLazyMockApiBundle:Mock:index.html.twig', array('mock' => $result));
+    }
+
+    public function sidebarAction()
+    {
+        $menu = array(
+            new MenuItem("Generale"),
+            new MenuItem("Home", $this->get('router')->generate('virhi_lazy_mock_api_ui_homepage')),
+            new MenuItem("List", $this->get('router')->generate('virhi_lazy_mock_api_ui_list')),
+        );
+
+        $viewParams = array(
+            'menu_item_list' => $menu,
+        );
+
+        return $this->render('VirhiAdminBundle:Theme:sidebar.html.twig', $viewParams);
+    }
+
+    public function headerAction()
+    {
+        $viewParams = array(
+            'user' =>  new User('vincent', 'rhino', 'developpeur', true),
+        );
+        return $this->render('VirhiAdminBundle:Theme:header.html.twig', $viewParams);
+    }
+
+    public function footerAction()
+    {
+        $viewParams = array(
+        );
+        return $this->render('VirhiAdminBundle:Theme:footer.html.twig', $viewParams);
     }
 }
--- src/Virhi/LazyMockApiBundle/VirhiLazyMockApiBundle.php
+++ src/Virhi/LazyMockApiBundle/VirhiLazyMockApiBundle.php
@@ -8,6 +8,6 @@ class VirhiLazyMockApiBundle extends Bundle
 {
     public function getParent()
     {
-        return 'VirhiAdminBundle';
+
     }
 }
